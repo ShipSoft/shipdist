7415edde34fafe617a99aff2d9d0b7c94ee81ad0
Philipp Munkes <p.munkes@cern.ch>
2017-07-17 10:26:11 +0200

    Update tauolapp recipe
    Update tauolapp recipe to to correct version of pythia and to have a working build process.
    
    Correct tauolapp.sh to have a correct build process

--- tauolapp.sh
+++ tauolapp.sh
@@ -5,14 +5,17 @@ tag: v1.1.5
 requires:
   - HepMC
   - ROOT
-  - pythia6
+  - pythia
   - lhapdf5
-env:
-  HEPMCLOCATION: "$HepMC_ROOT"
 ---
 #!/bin/sh
 
-$SOURCEDIR/configure --srcdir=$SOURCEDIR --with-hepmc=$HEPMC_ROOT --with-lhapdf=$LHAPDF5_ROOT --with-pythia8=$PYTHIA_ROOT --prefix=$INSTALLROOT CFLAGS="$CFLAGS" CXXFLAGS="$CFLAGS"
+export  HEPMCLOCATION="$HEPMC_ROOT"
+
+rsync -a $SOURCEDIR/* .
+
+./configure --with-hepmc=$HEPMC_ROOT --with-lhapdf=$LHAPDF5_ROOT --with-pythia8=$PYTHIA_ROOT --prefix=$INSTALLROOT CFLAGS="$CFLAGS" CXXFLAGS="$CFLAGS"
+
 make ${JOBS+-j $JOBS}
 make install
 
@@ -29,7 +32,7 @@ proc ModulesHelp { } {
 set version $PKGVERSION-@@PKGREVISION@$PKGHASH@@
 module-whatis "ALICE Modulefile for $PKGNAME $PKGVERSION-@@PKGREVISION@$PKGHASH@@"
 # Dependencies
-module load BASE/1.0 ROOT/$ROOT_VERSION-$ROOT_REVISION HepMC/$HEPMC_VERSION-$HEPMC_REVISION pythia6/$PYTHIA6_VERSION-$PYTHIA6_REVISION lhapdf5/$LHAPDF5_VERSION-$LHAPDF5_REVISION
+module load BASE/1.0 ROOT/$ROOT_VERSION-$ROOT_REVISION pythia/$PYTHIA_VERSION-$PYHIA_REVISION HepMC/$HEPMC_VERSION-$HEPMC_REVISION lhapdf5/$LHAPDF5_VERSION-$LHADPF5_REVISION
 # Our environment
 setenv TAUOLA_ROOT \$::env(BASEDIR)/$PKGNAME/\$version
 $([[ ${ARCHITECTURE:0:3} == osx ]] && echo "prepend-path DYLD_LIBRARY_PATH \$::env(TAUOLA_ROOT)/lib")
