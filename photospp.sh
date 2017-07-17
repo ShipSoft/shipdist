6e4013dce74da24f92471d16de1e55a4a994a68c
Philipp Munkes <p.munkes@cern.ch>
2017-07-17 14:44:04 +0200

    Add Support for PHOTOS++

---
+++ photospp.sh
@@ -0,0 +1,39 @@
+package: PHOTOSPP
+version: "%(tag_basename)s-ship%(defaults_upper)s"
+source: https://github.com/PMunkes/PHOTOSPP
+tag: v1.1.5
+requires:
+  - HepMC
+  - ROOT
+  - pythia
+  - Tauolapp
+---
+#!/bin/sh
+
+export  HEPMCLOCATION="$HEPMC_ROOT"
+
+rsync -a $SOURCEDIR/* .
+
+./configure --with-hepmc=$HEPMC_ROOT --with-pythia8=$PYTHIA_ROOT --with-tauola=$TAUOLAPP_ROOT --prefix=$INSTALLROOT CFLAGS="$CFLAGS" CXXFLAGS="$CFLAGS"
+
+make ${JOBS+-j $JOBS}
+make install
+
+# Modulefile
+MODULEDIR="$INSTALLROOT/etc/modulefiles"
+MODULEFILE="$MODULEDIR/$PKGNAME"
+mkdir -p "$MODULEDIR"
+cat > "$MODULEFILE" <<EoF
+#%Module1.0
+proc ModulesHelp { } {
+  global version
+  puts stderr "ALICE Modulefile for $PKGNAME $PKGVERSION-@@PKGREVISION@$PKGHASH@@"
+}
+set version $PKGVERSION-@@PKGREVISION@$PKGHASH@@
+module-whatis "ALICE Modulefile for $PKGNAME $PKGVERSION-@@PKGREVISION@$PKGHASH@@"
+# Dependencies
+module load BASE/1.0 ROOT/$ROOT_VERSION-$ROOT_REVISION pythia/$PYTHIA_VERSION-$PYHIA_REVISION HepMC/$HEPMC_VERSION-$HEPMC_REVISION  Tauolapp/$TAUOLAPP_VERSION-$TAUOLAPP_REVISION
+# Our environment
+setenv PHOTOSPP_ROOT \$::env(BASEDIR)/$PKGNAME/\$version
+$([[ ${ARCHITECTURE:0:3} == osx ]] && echo "prepend-path DYLD_LIBRARY_PATH \$::env(PHOTOSPP_ROOT)/lib")
+EoF
