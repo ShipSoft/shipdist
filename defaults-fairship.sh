8c9add25a7ad0f7dcc4dffad6884d7feaa9e751d
Philipp Munkes <p.munkes@cern.ch>
2017-07-27 14:48:52 +0200

    initial commit of default fairship

--- defaults-fairship.sh
+++ defaults-fairship.sh
@@ -1,7 +1,7 @@
 package: defaults-fairship
 version: v1
 env:
-  CXXFLAGS: "-fPIC -g -O2 -std=c++14"
+  CXXFLAGS: "-fPIC -g -O2 -std=c++11"
   CFLAGS: "-fPIC -g -O2"
   CMAKE_BUILD_TYPE: "RELWITHDEBINFO"
 overrides:
@@ -29,6 +29,8 @@ overrides:
       - Xdevel:(?!osx)
       - FreeType:(?!osx)
       - Python-modules
+      - pythia
+      - pythia6
   GSL:
     prefer_system_check: |
       printf "#include \"gsl/gsl_version.h\"\n#define GSL_V GSL_MAJOR_VERSION * 100 + GSL_MINOR_VERSION\n# if (GSL_V < 116)\n#error \"Cannot use system's gsl. Notice we only support versions from 1.16 (included)\"\n#endif\nint main(){}" | gcc  -I$(brew --prefix gsl)/include -xc++ - -o /dev/null
@@ -41,11 +43,12 @@ overrides:
     prefer_system_check: |
       which cmake && case `cmake --version | sed -e 's/.* //' | cut -d. -f1,2,3 | head -n1` in [0-2]*|3.[0-7].*) exit 1 ;; esac
   FairRoot:
-    version: dev
-    tag: dev
+    source: https://github.com/PMunkes/FairRoot
+    version: fairshipdev
+    tag: fairshipdev
   GEANT4:
     version: "%(tag_basename)s"
-    tag: v10.3.1-ship2
+    tag: v10.3.1-ship3
     source: https://github.com/PMunkes/geant4
     requires:
       - "GCC-Toolchain:(?!osx)"
@@ -54,6 +57,10 @@ overrides:
     env:
       G4INSTALL: "$GEANT4_ROOT"
       G4SYSTEM: "$(uname)-g++"
+  GEANT4_VMC:
+    version: "%(tag_basename)s"
+    tag: fairshipdev
+    source: https://github.com/PMunkes/geant4_vmc
   G4PY:
     version: "%(tag_basename)s"
     tag: v10.3.1-ship2
@@ -72,7 +79,22 @@ overrides:
       - lhapdf5
       - HepMC
       - boost
-
+  vgm:
+    tag: "4.4"
+  generators:
+    requires:  
+      - pythia6
+      - pythia
+      - PHOTOSPP
+      - GENIE
+  evtGen:
+    tag: fairshipdev
+  pythia6:
+    source: https://github.com/PMunkes/pythia6
+    tag: v6.4.28-ship1
+  GEANT3:
+    source: https://github.com/PMunkes/GEANT3
+    tag: master  
 ---
 # This file is included in any build recipe and it's only used to set
 # environment variables. Which file to actually include can be defined by the
