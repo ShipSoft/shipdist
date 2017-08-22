712dd890b0a98d80b15009e3b6c8ece32c24aeea
Philipp Munkes <p.munkes@cern.ch>
2017-08-22 17:10:13 +0200

    another small fix for log4cpp

--- log4cpp.sh
+++ log4cpp.sh
@@ -2,6 +2,8 @@ package: log4cpp
 version: "%(tag_basename)s%(defaults_upper)s"
 tag: REL_1_1_1_Nov_26_2013
 source: https://github.com/PMunkes/log4cpp
+requires:
+  - GCC-Toolchain:(?!osx)
 build_requires:
   - autotools
 ---
