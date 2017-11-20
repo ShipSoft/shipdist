b3e69dbb587b5d4833f481d72a25f35baafcec06
Thomas Ruf <Thomas.Ruf@cern.ch>
2017-11-20 11:29:51 +0100

    add scipy as dependency

--- python-modules.sh
+++ python-modules.sh
@@ -12,7 +12,7 @@ prepend_path:
   PYTHONPATH: $PYTHON_MODULES_ROOT/lib/python2.7/site-packages:$PYTHONPATH
 prefer_system: (?!slc5)
 prefer_system_check:
-  python -c 'import matplotlib,numpy,certifi,IPython,ipywidgets,ipykernel,notebook.notebookapp,metakernel,yaml';
+  python -c 'import matplotlib,numpy, scipy, certifi,IPython,ipywidgets,ipykernel,notebook.notebookapp,metakernel,yaml';
   if [ $? -ne 0 ]; then printf "Required Python modules are missing. You can install them with pip (better as root):\n  pip install matplotlib numpy certifi ipython ipywidgets ipykernel notebook metakernel pyyaml\n"; exit 1; fi
 ---
 #!/bin/bash -ex
@@ -52,6 +52,7 @@ for X in "mock==1.0.0"         \
          "ipykernel==4.5.0"    \
          "notebook==4.2.3"     \
          "metakernel==0.14.0"  \
+         "scipy=0.17.1"        \
          "pyyaml"
 do
   python -m pip install --user $X
