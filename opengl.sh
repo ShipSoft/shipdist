a925aa2ebc486da4e6c0b401ce5862558bba857e
Antonio <antonio.iuliano1993@gmail.com>
2018-04-09 15:38:54 +0200

    OpenGL requirement on Mac OS X
    In OS X include folders are different (see https://en.wikibooks.org/wiki/OpenGL_Programming/Installation/Mac), thus leading to the builder assuming you do not have the required package.
    
    I suppose this should solve the issue,
    Antonio

--- opengl.sh
+++ opengl.sh
@@ -6,6 +6,9 @@ system_requirement_missing: |
     * On Ubuntu-compatible systems you probably need: libglu1-mesa-dev
 system_requirement: ".*"
 system_requirement_check: |
-  printf "#include <GL/glu.h>\n" | cc -xc - -c -o /dev/null
+  case $ARCHITECTURE in 
+  osx*) printf "#include <OpenGL/glu.h>\n" | cc -xc - -c -o /dev/null;; 
+  *) printf "#include <GL/glu.h>\n" | cc -xc - -c -o /dev/null;;
+  esac
 ---
 
