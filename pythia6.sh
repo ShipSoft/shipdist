7e0254513bfa5244d9652fd71d0e749131586555
Thomas Ruf <Thomas.Ruf@cern.ch>
2018-04-20 08:47:49 +0200

    fixes issue with build on centos, latest version from alisw

--- pythia6.sh
+++ pythia6.sh
@@ -1,27 +1,21 @@
 # a pythia6 recipe based on the one from FairROOT
 package: pythia6
-version: "%(tag_basename)s%(defaults_upper)s"
+version: "%(tag_basename)s"
 tag: "428-alice1"
 source: https://github.com/alisw/pythia6.git
+requires:
+  - GCC-Toolchain:(?!osx)
 build_requires:
   - CMake
-env:
-  PYTHIA6: "$PYTHIA6_ROOT"
 ---
 #!/bin/sh
 
 cmake ${SOURCEDIR}                           \
       -DCMAKE_BUILD_TYPE=${CMAKE_BUILD_TYPE} \
-      -DCMAKE_INSTALL_PREFIX=${INSTALLROOT}
-
+      -DCMAKE_INSTALL_PREFIX=${INSTALLROOT}  \
+      -DCMAKE_INSTALL_LIBDIR=lib
 make ${JOBS+-j$JOBS}
 make install
-tar -c lib include | tar -x -C $INSTALLROOT 
-
-cd $INSTALLROOT/lib/
-ln -s libpythia6.so libPythia6.so
-cd $BUILDROOT/pythia6
-
 
 # Modulefile
 MODULEDIR="$INSTALLROOT/etc/modulefiles"
@@ -39,7 +33,6 @@ module-whatis "ALICE Modulefile for $PKGNAME $PKGVERSION-@@PKGREVISION@$PKGHASH@
 module load BASE/1.0
 # Our environment
 setenv PYTHIA6_ROOT \$::env(BASEDIR)/$PKGNAME/\$version
-setenv PYTHIA6 \$::env(PYTHIA6_ROOT)
 prepend-path LD_LIBRARY_PATH \$::env(PYTHIA6_ROOT)/lib
 prepend-path AGILE_GEN_PATH \$::env(PYTHIA6_ROOT)
 $([[ ${ARCHITECTURE:0:3} == osx ]] && echo "prepend-path DYLD_LIBRARY_PATH \$::env(PYTHIA6_ROOT)/lib")
