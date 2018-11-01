52c7d02cdf170e5e0f2dcd57f1d33cd0811ffd64
Thomas Ruf <Thomas.Ruf@cern.ch>
2018-11-01 09:15:03 +0100

    change to RELASE without Debug info

--- defaults-fairship.sh
+++ defaults-fairship.sh
@@ -3,7 +3,7 @@ version: v1
 env:
   CXXFLAGS: "-fPIC -g -O2 -std=c++11"
   CFLAGS: "-fPIC -g -O2"
-  CMAKE_BUILD_TYPE: "RELWITHDEBINFO"
+  CMAKE_BUILD_TYPE: "RELEASE"
 disable:
   - AliEn-Runtime
   - MonALISA-gSOAP-client
