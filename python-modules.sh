3bb4181f1378f5703a62d220aa35e9607b8db6a6
Jakob Blomer <jblomer@cern.ch>
2019-07-16 08:31:54 +0200

    remove SSL ENV from python

--- python-modules.sh
+++ python-modules.sh
@@ -7,7 +7,6 @@ requires:
 build_requires:
   - curl
 env:
-  SSL_CERT_FILE: "$(env PYTHONPATH=$PYTHON_MODULES_ROOT/lib/python$(python -c \"import distutils.sysconfig; print(distutils.sysconfig.get_python_version())\")/site-packages:$PYTHONPATH python -c \"import certifi; print certifi.where()\")"
 prepend_path:
   PYTHONPATH: $PYTHON_MODULES_ROOT/lib/python2.7/site-packages:$PYTHONPATH
 prefer_system: (?!slc5)
@@ -63,44 +62,6 @@ do
 done
 unset PYTHONUSERBASE
 
-#
-## Install matplotlib (quite tricky)
-#MATPLOTLIB_TAG="2.2.4"
-#if [[ $ARCHITECTURE != slc* ]]; then
-#  # Simply get it via pip in most cases
-#  env PYTHONUSERBASE=$INSTALLROOT pip install --user "matplotlib==$MATPLOTLIB_TAG"
-#else
-#
-#  # We are on a RHEL-compatible OS. We compile it ourselves, and link it to our dependencies
-#
-#  # Check if we can enable the Tk interface
-#  python -c 'import _tkinter' && MATPLOTLIB_TKAGG=True || MATPLOTLIB_TKAGG=False
-#  MATPLOTLIB_URL="https://github.com/matplotlib/matplotlib/archive/v${MATPLOTLIB_TAG}.tar.gz"  # note the "v"
-#  curl -SsL "$MATPLOTLIB_URL" | tar xzf -
-#  cd matplotlib-*
-#  cat > setup.cfg <<EOF
-#[directories]
-#basedirlist  = ${FREETYPE_ROOT:+$PWD/fake_freetype_root,$FREETYPE_ROOT,}${LIBPNG_ROOT:+$LIBPNG_ROOT,}${ZLIB_ROOT:+$ZLIB_ROOT,}/usr/X11R6,$(freetype-config --prefix),$(libpng-config --prefix)
-#[gui_support]
-#gtk = False
-#gtkagg = False
-#tkagg = $MATPLOTLIB_TKAGG
-#wxagg = False
-#macosx = False
-#EOF
-#
-#  # matplotlib wants include files in <PackageRoot>/include, but this is not the case for FreeType
-#  if [[ $FREETYPE_ROOT ]]; then
-#    mkdir fake_freetype_root
-#    ln -nfs $FREETYPE_ROOT/include/freetype2 fake_freetype_root/include
-#  fi
-#
-#  export PYTHONPATH="$INSTALLROOT/lib/python2.7/site-packages"
-#    python setup.py build
-#    python setup.py install --prefix "$INSTALLROOT"
-#  unset PYTHONPATH
-#fi
-
 # Test if matplotlib can be loaded
 env PYTHONPATH="$INSTALLROOT/lib/python2.7/site-packages" python -c 'import matplotlib'
 
@@ -142,5 +103,4 @@ prepend-path LD_LIBRARY_PATH $::env(PYTHON_MODULES_ROOT)/lib
 $([[ ${ARCHITECTURE:0:3} == osx ]] && echo "prepend-path DYLD_LIBRARY_PATH $::env(PYTHON_MODULES_ROOT)/lib64" && \
                                       echo "prepend-path DYLD_LIBRARY_PATH $::env(PYTHON_MODULES_ROOT)/lib")
 prepend-path PYTHONPATH $::env(PYTHON_MODULES_ROOT)/lib/python$PYVER/site-packages
-setenv SSL_CERT_FILE  [exec python -c "import certifi; print certifi.where()"]
 EoF
