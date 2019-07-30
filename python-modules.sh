dfe256d02ee306c004b4c6c93693383e7b162fb8
Jakob Blomer <jblomer@cern.ch>
2019-07-30 11:32:23 +0200

    fix python-modules on py2

--- python-modules.sh
+++ python-modules.sh
@@ -62,44 +62,7 @@ do
   python -m pip install --user $X
 done
 unset PYTHONUSERBASE
-#
-## Install matplotlib (quite tricky)
-#MATPLOTLIB_TAG="3.0.3"
-#if [[ $ARCHITECTURE != slc* ]]; then
-#  # Simply get it via pip in most cases
-#  env PYTHONUSERBASE=$INSTALLROOT pip install --user "matplotlib==$MATPLOTLIB_TAG"
-#else
-#
-#  # We are on a RHEL-compatible OS. We compile it ourselves, and link it to our dependencies
-#
-#  # Check if we can enable the Tk interface
-#  python -c 'import _tkinter' && MATPLOTLIB_TKAGG=True || MATPLOTLIB_TKAGG=False
-#  MATPLOTLIB_URL="https://github.com/matplotlib/matplotlib/archive/v${MATPLOTLIB_TAG}.tar.gz"  # note the "v"
-#  curl -SsL "$MATPLOTLIB_URL" | tar xzf -
-#  cd matplotlib-*
-#  cat > setup.cfg <<EOF
-#[directories]
-#basedirlist  = ${FREETYPE_ROOT:+$PWD/fake_freetype_root,$FREETYPE_ROOT,}${LIBPNG_ROOT:+$LIBPNG_ROOT,}${ZLIB_ROOT:+$ZLIB_ROOT,}/usr/X11R6,$(freetype-config --prefix),$(libpng-config --prefix)
-#[gui_support]
-#gtk = False
-#gtkagg = False
-#tkagg = $MATPLOTLIB_TKAGG
-#wxagg = False
-#macosx = False
-#EOF
-#
-#  # matplotlib wants include files in <PackageRoot>/include, but this is not the case for FreeType
-#  if [[ $FREETYPE_ROOT ]]; then
-#    mkdir fake_freetype_root
-#    ln -nfs $FREETYPE_ROOT/include/freetype2 fake_freetype_root/include
-#  fi
-#
-#  export PYTHONPATH="$INSTALLROOT/lib/python/site-packages"
-#    python setup.py build
-#    python setup.py install --prefix "$INSTALLROOT"
-#  unset PYTHONPATH
-#fi
-#                                                            
+                                                            
 # Test if matplotlib can be loaded
 env PYTHONPATH="$INSTALLROOT/lib/python2.7/site-packages" python -c 'import matplotlib'
 
