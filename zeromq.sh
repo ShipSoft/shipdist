951b4613899efde7c7329e4114df8540c35517d5
ThomasRuf <Thomas.Ruf@cern.ch>
2021-02-10 16:12:38 +0100

    copied from alidist master

--- zeromq.sh
+++ zeromq.sh
@@ -1,13 +1,14 @@
 package: ZeroMQ
-version: v4.1.5
-source: https://github.com/alisw/zeromq
+version: v4.3.3
+source: https://github.com/zeromq/libzmq
 requires:
   - "GCC-Toolchain:(?!osx)"
 build_requires:
-  - autotools
+  - "autotools:(slc6|slc7)"
+  - alibuild-recipe-tools
 prefer_system: (?!slc5.*)
 prefer_system_check: |
-  printf "#include <zmq.h>\n#if(ZMQ_VERSION < 40103)\n#error \"zmq version >= 4.1.3 needed\"\n#endif\n int main(){}" | gcc -I$(brew --prefix zeromq)/include $([[ -d $(brew --prefix zeromq) ]] || echo "-l:libzmq.a") -xc++ - -o /dev/null 2>&1
+  printf "#include <zmq.h>\n#if(ZMQ_VERSION < 40105)\n#error \"zmq version >= 4.1.5 needed\"\n#endif\n int main(){}" | c++ -I$(brew --prefix zeromq)/include -xc++ - -o /dev/null 2>&1
 ---
 #!/bin/sh
 
@@ -18,25 +19,10 @@ cd $BUILDDIR
 ./configure --prefix=$INSTALLROOT          \
             --disable-dependency-tracking
 
-make ${JOBS+-j $JOBS}
-make install
+make ${JOBS+-j $JOBS} install
 
 # Modulefile
 MODULEDIR="$INSTALLROOT/etc/modulefiles"
 MODULEFILE="$MODULEDIR/$PKGNAME"
 mkdir -p "$MODULEDIR"
-cat > "$MODULEFILE" <<EoF
-#%Module1.0
-proc ModulesHelp { } {
-  global version
-  puts stderr "ALICE Modulefile for $PKGNAME $PKGVERSION-@@PKGREVISION@$PKGHASH@@"
-}
-set version $PKGVERSION-@@PKGREVISION@$PKGHASH@@
-module-whatis "ALICE Modulefile for $PKGNAME $PKGVERSION-@@PKGREVISION@$PKGHASH@@"
-# Dependencies
-module load BASE/1.0 ${SODIUM_ROOT:+sodium/$SODIUM_VERSION-$SODIUM_REVISION} ${GCC_TOOLCHAIN_ROOT:+GCC-Toolchain/$GCC_TOOLCHAIN_VERSION-$GCC_TOOLCHAIN_REVISION}
-# Our environment
-setenv ZEROMQ_ROOT \$::env(BASEDIR)/$PKGNAME/\$version
-prepend-path LD_LIBRARY_PATH \$::env(ZEROMQ_ROOT)/lib
-$([[ ${ARCHITECTURE:0:3} == osx ]] && echo "prepend-path DYLD_LIBRARY_PATH \$::env(ZEROMQ_ROOT)/lib")
-EoF
+alibuild-generate-module --lib > $MODULEFILE
