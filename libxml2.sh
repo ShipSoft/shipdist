75f6dfd693fb03c625262cac9a6ff1d63cc918ed
siilieva <sii@cern.ch>
2021-06-04 09:58:24 +0200

    Apply the latest recipe from alidist for libxm2.

--- libxml2.sh
+++ libxml2.sh
@@ -1,15 +1,15 @@
 package: libxml2
 version: "%(tag_basename)s"
-tag: v2.9.12
+tag: v2.9.3
 build_requires:
-  - autotools
+  - "autotools:(slc6|slc7)"
   - zlib
   - "GCC-Toolchain:(?!osx)"
-source: https://gitlab.gnome.org/GNOME/libxml2.git
-#prefer_system: "(?!slc5)"
-#prefer_system_check: |
-#  xml2-config --version;
-#  if [ $? -ne 0 ]; then printf "libxml2 not found.\n * On RHEL-compatible systems you probably need: libxml2 libxml2-devel\n * On Ubuntu-compatible systems you probably need: libxml2 libxml2-dev"; exit 1; fi
+source: https://github.com/alisw/libxml2.git
+prefer_system: "(?!slc5)"
+prefer_system_check: |
+  xml2-config --version;
+  if [ $? -ne 0 ]; then printf "libxml2 not found.\n * On RHEL-compatible systems you probably need: libxml2 libxml2-devel\n * On Ubuntu-compatible systems you probably need: libxml2 libxml2-dev"; exit 1; fi
 ---
 #!/bin/sh
 echo "Building ALICE libxml. To avoid this install libxml development package."
@@ -21,7 +21,6 @@ autoreconf -i
 
 make ${JOBS+-j $JOBS}
 make install
-
 # Modulefile
 mkdir -p etc/modulefiles
 cat > etc/modulefiles/$PKGNAME <<EoF
@@ -33,12 +32,12 @@ proc ModulesHelp { } {
 set version $PKGVERSION-@@PKGREVISION@$PKGHASH@@
 module-whatis "ALICE Modulefile for $PKGNAME $PKGVERSION-@@PKGREVISION@$PKGHASH@@"
 # Dependencies
-module load BASE/1.0 ${ZLIB_VERSION:+zlib/$ZLIB_VERSION-$ZLIB_REVISION}
+module load BASE/1.0 ${ZLIB_REVISION:+zlib/$ZLIB_VERSION-$ZLIB_REVISION}
 # Our environment
 setenv LIBXML2_VERSION \$version
-setenv LIBXML2_ROOT \$::env(BASEDIR)/$PKGNAME/\$::env(LIBXML2_VERSION)
-prepend-path PATH \$::env(LIBXML2_ROOT)/bin
-prepend-path LD_LIBRARY_PATH \$::env(LIBXML2_ROOT)/lib
-$([[ ${ARCHITECTURE:0:3} == osx ]] && echo "prepend-path DYLD_LIBRARY_PATH \$::env(LIBXML2_ROOT)/lib")
+set LIBXML2_ROOT \$::env(BASEDIR)/$PKGNAME/\$::env(LIBXML2_VERSION)
+setenv LIBXML2_ROOT \$LIBXML2_ROOT
+prepend-path PATH \$LIBXML2_ROOT/bin
+prepend-path LD_LIBRARY_PATH \$LIBXML2_ROOT/lib
 EoF
 mkdir -p $INSTALLROOT/etc/modulefiles && rsync -a --delete etc/modulefiles/ $INSTALLROOT/etc/modulefiles
