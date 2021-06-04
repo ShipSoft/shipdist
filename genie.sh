33aa1c6317e031989c71f4ce33e31607d9816aca
siilieva <sii@cern.ch>
2021-06-04 10:13:22 +0200

    GENIE installation using lhapdf6 package.

--- genie.sh
+++ genie.sh
@@ -5,7 +5,7 @@ source: https://github.com/GENIE-MC/Generator.git
 requires:
   - GCC-Toolchain
   - ROOT
-  - lhapdf5
+  - lhapdf
   - pythia6
   - log4cpp
   - GSL
@@ -22,7 +22,7 @@ export GENIE="$BUILDDIR"
 rsync -a $SOURCEDIR/* $BUILDDIR
 ls -alh $BUILDDIR
 $BUILDDIR/configure --prefix=$INSTALLROOT \
-		    --enable-lhapdf \
+		    --enable-lhapdf6 \
 		    --enable-validation-tools \
 		    --enable-test \
 		    --enable-numi\
@@ -32,8 +32,8 @@ $BUILDDIR/configure --prefix=$INSTALLROOT \
 		    --enable-pyhia6 \
 		    --enable-mathmore \
       		    --with-pythia6-lib=$PYTHIA6_ROOT/lib/ \
-		    --with-lhapdf-lib=$LHAPDF5_ROOT/lib/ \
-		    --with-lhapdf-inc=$LHAPDF5_ROOT/include/ \
+		    --with-lhapdf-lib=$LHAPDF_ROOT/lib/ \
+		    --with-lhapdf-inc=$LHAPDF_ROOT/include/ \
 		    --with-libxml2-lib=$LIBXML2_ROOT/lib/ \
 		    --with-libxml2-inc=$LIBXML2_ROOT/include/libxml2 \
 		    --with-log4cpp-inc=$LOG4CPP_ROOT/include/ \
@@ -58,7 +58,7 @@ rsync -a src/* $INSTALLROOT/genie/src
 mkdir -p $INSTALLROOT/genie/inc
 rsync -a src/*/*/*.h $INSTALLROOT/genie/inc
 
-cp $INSTALLROOT/genie/data/evgen/pdfs/GRV98lo_patched.LHgrid $LHAPDF5_ROOT/share/lhapdf
+#cp $INSTALLROOT/genie/data/evgen/pdfs/GRV98lo_patched.LHgrid $LHAPDF5_ROOT/share/lhapdf
 
 # Modulefile
 MODULEDIR="$INSTALLROOT/etc/modulefiles"
@@ -73,7 +73,7 @@ proc ModulesHelp { } {
 set version $PKGVERSION-@@PKGREVISION@$PKGHASH@@
 module-whatis "ALICE Modulefile for $PKGNAME $PKGVERSION-@@PKGREVISION@$PKGHASH@@"
 # Dependencies
-module load BASE/1.0 ROOT/$ROOT_VERSION-$ROOT_REVISION pythia6/$PYTHIA6_VERSION-$PYTHIA6_REVISION lhapdf5/$LHAPDF5_VERSION-$LHAPDF5_REVISION log4cpp/$LOG4CPP_VERSION-$LOG4CPP_REVISION ${LIBXML2:+libxml2/$LIBXML2_VERSION-$LIBXML2_REVISION} ${GSL_VERSION:+GSL/$GSL_VERSION-$GSL_REVISION}
+module load BASE/1.0 ROOT/$ROOT_VERSION-$ROOT_REVISION pythia6/$PYTHIA6_VERSION-$PYTHIA6_REVISION lhapdf/$LHAPDF_VERSION-$LHAPDF_REVISION log4cpp/$LOG4CPP_VERSION-$LOG4CPP_REVISION ${LIBXML2:+libxml2/$LIBXML2_VERSION-$LIBXML2_REVISION} ${GSL_VERSION:+GSL/$GSL_VERSION-$GSL_REVISION}
 # Our environment
 setenv GENIE_ROOT \$::env(BASEDIR)/$PKGNAME/\$version
 setenv GENIE \$::env(GENIE_ROOT)/genie
