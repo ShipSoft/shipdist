dcdb2c7b19184a2478568a7e2f6013d6d4854e67
ThomasRuf <Thomas.Ruf@cern.ch>
2021-09-21 14:00:11 +0200

    update recipe to have python bindings

--- defaults-release.sh
+++ defaults-release.sh
@@ -30,7 +30,7 @@ overrides:
       which gfortran || { echo "gfortran missing"; exit 1; }
       which cc && test -f $(dirname $(which cc))/c++ && printf "#define GCCVER ((__GNUC__ << 16)+(__GNUC_MINOR__ << 8)+(__GNUC_PATCHLEVEL__))\n#if (GCCVER < 0x060000 || GCCVER > 0x100000)\n#error \"System's GCC cannot be used: we need GCC 6.X. We are going to compile our own version.\"\n#endif\n" | cc -xc++ - -c -o /dev/null
   XRootD:
-    tag: v4.12.1
+    tag: v5.3.1
   ROOT:
     tag: "v6-24-00"
     source: https://github.com/root-project/root
