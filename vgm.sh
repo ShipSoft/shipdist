084c6f7e68de174db59ff01729678098ee216140
Oliver Lantwin <oliver.lantwin@cern.ch>
2024-05-27 10:55:59 +0200

    vgm: update source to upstream and update to 5-2

--- vgm.sh
+++ vgm.sh
@@ -1,24 +1,29 @@
 package: vgm
-version: "%(tag_basename)s%(defaults_upper)s"
-tag: "4.3-alice1"
-source: https://github.com/alisw/vgm.git
+version: "%(tag_basename)s"
+tag: "v5-2"
+source: https://github.com/vmc-project/vgm
 requires:
   - ROOT
   - GEANT4
-  - XercesC
 build_requires:
   - CMake
 ---
 #!/bin/bash -e
+
 cmake "$SOURCEDIR" \
-  -DCMAKE_BUILD_TYPE=${CMAKE_BUILD_TYPE} \
-  -DCMAKE_INSTALL_LIBDIR="lib"           \
-  -DCMAKE_INSTALL_PREFIX="$INSTALLROOT"  \
-  -DWITH_EXAMPLES=OFF                \
-  -DWITH_TEST=OFF                     \
-  -DBUILD_SHARED_LIBS=OFF
+  -DCMAKE_CMAKE_BUILD_TYPE=${CMAKE_BUILD_TYPE} \
+  -DCMAKE_INSTALL_LIBDIR="lib"                 \
+  -DCMAKE_INSTALL_PREFIX="$INSTALLROOT"        \
+  -DCMAKE_POLICY_DEFAULT_CMP0074=NEW \
+  ${CXXSTD:+-DCMAKE_CXX_STANDARD=$CXXSTD}
 
 make ${JOBS+-j $JOBS} install
+
+# Relocation of .cmake files
+for CMAKE in $(find "$INSTALLROOT/lib" -name '*.cmake'); do
+  sed -ideleteme -e "s!$ROOTSYS!\$ENV{ROOTSYS}!g; s!$G4INSTALL!\$ENV{G4INSTALL}!g" "$CMAKE"
+done
+find "$INSTALLROOT/lib" -name '*deleteme' -delete || true
 
 # Modulefile
 MODULEDIR="$INSTALLROOT/etc/modulefiles"
@@ -35,7 +40,7 @@ module-whatis "ALICE Modulefile for $PKGNAME $PKGVERSION-@@PKGREVISION@$PKGHASH@
 # Dependencies
 module load BASE/1.0 GEANT4/$GEANT4_VERSION-$GEANT4_REVISION ROOT/$ROOT_VERSION-$ROOT_REVISION
 # Our environment
-setenv VGM_ROOT \$::env(BASEDIR)/$PKGNAME/\$version
-prepend-path LD_LIBRARY_PATH \$::env(VGM_ROOT)/lib
-$([[ ${ARCHITECTURE:0:3} == osx ]] && echo "prepend-path DYLD_LIBRARY_PATH \$::env(VGM_ROOT)/lib")
+set VGM_ROOT \$::env(BASEDIR)/$PKGNAME/\$version
+setenv VGM_ROOT \$VGM_ROOT
+prepend-path LD_LIBRARY_PATH \$VGM_ROOT/lib
 EoF
