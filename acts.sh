217ebd90ab78bcfbef46060911b05eac2e029c89
James <james.webb@cern.ch>
2025-08-06 01:43:43 +0200

    Implementation of ACTS build recipes.

---
+++ acts.sh
@@ -0,0 +1,61 @@
+package: acts
+source: https://github.com/webbjm/acts.git
+version: shipdist-build
+requires:
+  - ROOT
+  - GEANT4
+  - pythia
+  - HepMC3
+build-requires:
+  - CMake
+  - boost
+  - alibuild-recipe-tools
+---
+
+#!/bin/bash -e
+
+cmake "$SOURCEDIR" -DCMAKE_INSTALL_PREFIX=$INSTALLROOT \
+  -DCMAKE_SKIP_RPATH=TRUE \
+  -DCMAKE_PREFIX_PATH="${PYTHIA_ROOT}" \
+  -DGeant4_DIR=${GEANT4_ROOT}/lib           \
+  -DACTS_BUILD_FATRAS=ON \
+  -DACTS_BUILD_PLUGIN_GEANT4=ON \
+  -DACTS_BUILD_FATRAS_GEANT4=ON \
+  -DACTS_BUILD_ANALYSIS_APPS=ON \
+  -DACTS_BUILD_PLUGIN_TGEO=ON \
+  -DACTS_BUILD_EXAMPLES=ON \
+  -DACTS_BUILD_EXAMPLES_PYTHON_BINDINGS=ON \
+  -DACTS_BUILD_EXAMPLES_PYTHIA8=ON \
+  -DACTS_BUILD_EXAMPLES_GEANT4=ON \
+  -DCMAKE_VERBOSE_MAKEFILE=1 \
+  -DCMAKE_BUILD_TYPE=${CMAKE_BUILD_TYPE}
+
+
+cmake --build . -- ${JOBS:+-j$JOBS} install
+
+[[ -d $INSTALLROOT/lib64 ]] && [[ ! -d $INSTALLROOT/lib ]] && ln -sf ${INSTALLROOT}/lib64 $INSTALLROOT/lib
+
+# Modulefile
+MODULEDIR="$INSTALLROOT/etc/modulefiles"
+MODULEFILE="$MODULEDIR/$PKGNAME"
+mkdir -p "$MODULEDIR"
+alibuild-generate-module --bin --lib > "${MODULEFILE}"
+cat > "$MODULEFILE" <<EoF
+#%Module1.0
+proc ModulesHelp { } {
+  global version
+  puts stderr "ALICE Modulefile for $PKGNAME $PKGVERSION-@@PKGREVISION@$PKGHASH@@"
+}
+set version $PKGVERSION-@@PKGREVISION@$PKGHASH@@
+module-whatis "ALICE Modulefile for $PKGNAME $PKGVERSION-@@PKGREVISION@$PKGHASH@@"
+# Dependencies
+if ![ is-loaded 'BASE/1.0' ] {
+ module load BASE/1.0
+}
+
+if ![ is-loaded "ROOT/$ROOT_VERSION-$ROOT_REVISION" ] { module load "ROOT/$ROOT_VERSION-$ROOT_REVISION"}
+set ACTS_ROOT \$::env(BASEDIR)/$PKGNAME/\$version 
+prepend-path LD_LIBRARY_PATH \$ACTS_ROOT/lib
+prepend-path ROOT_INCLUDE_PATH \$ACTS_ROOT/include
+$([[ ${ARCHITECTURE:0:3} == osx ]] && echo "prepend-path DYLD_LIBRARY_PATH \$::env(ACTS_ROOT)/lib")
+EoF
--- hepmc3.sh
+++ hepmc3.sh
@@ -1,18 +1,23 @@
 package: HepMC3
 version: "%(tag_basename)s"
 source: https://gitlab.cern.ch/hepmc/HepMC3.git
-tag: beta2.0
+tag: master
 requires:
   - ROOT
 build_requires:
   - CMake
   - GCC-Toolchain:(?!osx.*)
+env:
+  "HEPMC3": $HEPMC3_ROOT
+prepend_path:
+  "ROOT_INCLUDE_PATH": "$HEPMC3_ROOT/include"
 ---
 #!/bin/bash -e
 
 cmake  $SOURCEDIR                           \
        -DCMAKE_INSTALL_PREFIX=$INSTALLROOT  \
-       -DROOT_DIR=$ROOT_ROOT
+       -DCMAKE_INSTALL_LIBDIR=lib           \
+       -DROOT_DIR=$ROOT_ROOT 
 
 make ${JOBS+-j $JOBS}
 make install
@@ -30,10 +35,24 @@ proc ModulesHelp { } {
 set version $PKGVERSION-@@PKGREVISION@$PKGHASH@@
 module-whatis "ALICE Modulefile for $PKGNAME $PKGVERSION-@@PKGREVISION@$PKGHASH@@"
 # Dependencies
-module load BASE/1.0 ${GCC_TOOLCHAIN_ROOT:+GCC-Toolchain/$GCC_TOOLCHAIN_VERSION-$GCC_TOOLCHAIN_REVISION} ${ROOT_VERSION:+ROOT/$ROOT_VERSION-$ROOT_REVISION}
+#module load BASE/1.0 ${GCC_TOOLCHAIN_ROOT:+GCC-Toolchain/$GCC_TOOLCHAIN_VERSION-$GCC_TOOLCHAIN_REVISION} ${ROOT_VERSION:+ROOT/$ROOT_VERSION-$ROOT_REVISION}
+#%Module1.0
+proc ModulesHelp { } {
+  global version
+  puts stderr "ALICE Modulefile for $PKGNAME $PKGVERSION-@@PKGREVISION@$PKGHASH@@"
+}
+set version $PKGVERSION-@@PKGREVISION@$PKGHASH@@
+module-whatis "ALICE Modulefile for $PKGNAME $PKGVERSION-@@PKGREVISION@$PKGHASH@@"
+# Dependencies
+if ![ is-loaded 'BASE/1.0' ] {
+ module load BASE/1.0
+}
+
+if ![ is-loaded "ROOT/$ROOT_VERSION-$ROOT_REVISION" ] { module load "ROOT/$ROOT_VERSION-$ROOT_REVISION"}
 # Our environment
-setenv HEPMC3_ROOT \$::env(BASEDIR)/$PKGNAME/\$version
-prepend-path PATH \$::env(HEPMC3_ROOT)/bin
-prepend-path LD_LIBRARY_PATH \$::env(HEPMC3_ROOT)/lib
+set HEPMC3_ROOT \$::env(BASEDIR)/$PKGNAME/\$version
+prepend-path PATH \$HEPMC3_ROOT/bin
+prepend-path LD_LIBRARY_PATH \$HEPMC3_ROOT/lib
+prepend-path ROOT_INCLUDE_PATH \$HEPMC3_ROOT/include
 $([[ ${ARCHITECTURE:0:3} == osx ]] && echo "prepend-path DYLD_LIBRARY_PATH \$::env(HEPMC3_ROOT)/lib")
 EoF
