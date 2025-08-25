a32608dd01ff7890946f6049612453db97c96479
James <james.webb@cern.ch>
2025-08-25 13:56:52 +0200

    Addressed linter comments.

--- hepmc3.sh
+++ hepmc3.sh
@@ -7,12 +7,17 @@ requires:
 build_requires:
   - CMake
   - GCC-Toolchain:(?!osx.*)
+  - alibuild-recipe-tools
 env:
   "HEPMC3": $HEPMC3_ROOT
 prepend_path:
   "ROOT_INCLUDE_PATH": "$HEPMC3_ROOT/include"
 ---
 #!/bin/bash -e
+MODULEDIR="$INSTALLROOT/etc/modulefiles"
+MODULEFILE="$MODULEDIR/$PKGNAME"
+mkdir -p "$MODULEDIR"
+alibuild-generate-module --bin --lib > "${MODULEFILE}"
 
 cmake  $SOURCEDIR                           \
        -DCMAKE_INSTALL_PREFIX=$INSTALLROOT  \
@@ -23,36 +28,10 @@ make ${JOBS+-j $JOBS}
 make install
 
 # Modulefile
-MODULEDIR="$INSTALLROOT/etc/modulefiles"
-MODULEFILE="$MODULEDIR/$PKGNAME"
-mkdir -p "$MODULEDIR"
 cat > "$MODULEFILE" <<EoF
-#%Module1.0
-proc ModulesHelp { } {
-  global version
-  puts stderr "ALICE Modulefile for $PKGNAME $PKGVERSION-@@PKGREVISION@$PKGHASH@@"
-}
-set version $PKGVERSION-@@PKGREVISION@$PKGHASH@@
-module-whatis "ALICE Modulefile for $PKGNAME $PKGVERSION-@@PKGREVISION@$PKGHASH@@"
-# Dependencies
-#module load BASE/1.0 ${GCC_TOOLCHAIN_ROOT:+GCC-Toolchain/$GCC_TOOLCHAIN_VERSION-$GCC_TOOLCHAIN_REVISION} ${ROOT_VERSION:+ROOT/$ROOT_VERSION-$ROOT_REVISION}
-#%Module1.0
-proc ModulesHelp { } {
-  global version
-  puts stderr "ALICE Modulefile for $PKGNAME $PKGVERSION-@@PKGREVISION@$PKGHASH@@"
-}
-set version $PKGVERSION-@@PKGREVISION@$PKGHASH@@
-module-whatis "ALICE Modulefile for $PKGNAME $PKGVERSION-@@PKGREVISION@$PKGHASH@@"
-# Dependencies
-if ![ is-loaded 'BASE/1.0' ] {
- module load BASE/1.0
-}
-
-if ![ is-loaded "ROOT/$ROOT_VERSION-$ROOT_REVISION" ] { module load "ROOT/$ROOT_VERSION-$ROOT_REVISION"}
 # Our environment
 set HEPMC3_ROOT \$::env(BASEDIR)/$PKGNAME/\$version
 prepend-path PATH \$HEPMC3_ROOT/bin
 prepend-path LD_LIBRARY_PATH \$HEPMC3_ROOT/lib
 prepend-path ROOT_INCLUDE_PATH \$HEPMC3_ROOT/include
-$([[ ${ARCHITECTURE:0:3} == osx ]] && echo "prepend-path DYLD_LIBRARY_PATH \$::env(HEPMC3_ROOT)/lib")
 EoF
