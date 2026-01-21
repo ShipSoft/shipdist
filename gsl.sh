97d0d0343299dea44892609c5a2f91f6e192d1bb
Oliver Lantwin <oliver.lantwin@cern.ch>
2026-01-21 12:53:15 +0100

    deps: bump GSL to 2.8 for ROOT compatibility

--- gsl.sh
+++ gsl.sh
@@ -1,45 +1,32 @@
 package: GSL
-version: "v1.16%(defaults_upper)s"
+version: "v2.8"
+tag: "v2.8"
 source: https://github.com/alisw/gsl
-tag: "release-1-16"
-build_requires:
-  - autotools
 requires:
   - "GCC-Toolchain:(?!osx)"
+build_requires:
+  - "autotools:(slc6|slc7)"
+  - alibuild-recipe-tools
 prefer_system: (?!slc5)
 prefer_system_check: |
-  printf "#include \"gsl/gsl_version.h\"\n#define GSL_V GSL_MAJOR_VERSION * 100 + GSL_MINOR_VERSION\n# if (GSL_V < 116)\n#error \"Cannot use system's gsl. Notice we only support versions from 1.16 (included)\"\n#endif\nint main(){}"\
-   | gcc  -I"$GSL_ROOT/include" -xc++ - -o /dev/null
+  printf "#include \"gsl/gsl_version.h\"\n#define GSL_V GSL_MAJOR_VERSION * 100 + GSL_MINOR_VERSION\n# if (GSL_V < 116)\n#error \"Cannot use system's gsl. Notice we only support versions from 1.16 (included)\"\n#endif\nint main(){}" | cc -xc - -I$(brew --prefix gsl)/include  -c -o /dev/null
 ---
 #!/bin/bash -e
-rsync -a --exclude '**/.git' --delete $SOURCEDIR/ $BUILDDIR
+rsync -a --chmod=ug=rwX --exclude .git --delete-excluded $SOURCEDIR/ $BUILDDIR/
 # Do not build documentation
-perl -p -i -e "s/doc//" Makefile.am
-perl -p -i -e "s|doc/Makefile||" configure.ac
+sed -i.bak -e "s/doc//" Makefile.am
+sed -i.bak -e "s|doc/Makefile||" configure.ac
 autoreconf -f -v -i
 ./configure --prefix="$INSTALLROOT" \
             --enable-maintainer-mode
 make ${JOBS:+-j$JOBS}
 make ${JOBS:+-j$JOBS} install
 rm -fv $INSTALLROOT/lib/*.la
-
 # Modulefile
 MODULEDIR="$INSTALLROOT/etc/modulefiles"
 MODULEFILE="$MODULEDIR/$PKGNAME"
 mkdir -p "$MODULEDIR"
-cat > "$MODULEFILE" <<EoF
-#%Module1.0
-proc ModulesHelp { } {
-  global version
-  puts stderr "ALICE Modulefile for $PKGNAME $PKGVERSION-@@PKGREVISION@$PKGHASH@@"
-}
-set version $PKGVERSION-@@PKGREVISION@$PKGHASH@@
-module-whatis "ALICE Modulefile for $PKGNAME $PKGVERSION-@@PKGREVISION@$PKGHASH@@"
-# Dependencies
-module load BASE/1.0 ${GCC_TOOLCHAIN_ROOT:+GCC-Toolchain/$GCC_TOOLCHAIN_VERSION-$GCC_TOOLCHAIN_REVISION}
-# Our environment
-setenv GSL_ROOT \$::env(BASEDIR)/$PKGNAME/\$version
-prepend-path LD_LIBRARY_PATH \$::env(GSL_ROOT)/lib
-$([[ ${ARCHITECTURE:0:3} == osx ]] && echo "prepend-path DYLD_LIBRARY_PATH \$::env(GSL_ROOT)/lib")
-prepend-path PATH \$::env(GSL_ROOT)/bin
+alibuild-generate-module --bin --lib > $MODULEFILE
+cat >> "$MODULEFILE" <<EoF
+prepend-path ROOT_INCLUDE_PATH \$PKG_ROOT/include
 EoF
