9c3fd04cf6298e97e3e196213cd552fbda6fa816
Oliver Lantwin <oliver.lantwin@cern.ch>
2026-01-21 15:58:57 +0100

    deps: bump libpng to 1.6.47, add ninja

--- libpng.sh
+++ libpng.sh
@@ -1,43 +1,31 @@
 package: libpng
-version: v1.6.18
+version: v1.6.47
 requires:
- - zlib
+  - zlib
 build_requires:
- - CMake
-source: https://git.code.sf.net/p/libpng/code
+  - CMake
+  - ninja
+  - alibuild-recipe-tools
+source: https://github.com/pnggroup/libpng
 prefer_system: (?!slc5)
 prefer_system_check: |
-  printf "#include <png.h>\n" | gcc -xc++ - `libpng-config --cflags` -c -M 2>&1
+  printf "#include <png.h>\n" | c++ -xc++ - `libpng-config --cflags` -c -M 2>&1
   if [ $? -ne 0 ]; then printf "libpng was not found.\n * On RHEL-compatible systems you probably need: libpng libpng-devel\n * On Ubuntu-compatible systems you probably need: libpng12-0 libpng12-dev"; exit 1; fi
 ---
 #!/bin/bash -ex
-rsync -a $SOURCEDIR/ .
-cmake .                                        \
+cmake ${SOURCEDIR}                             \
+    -G Ninja                                   \
     -DCMAKE_INSTALL_PREFIX:PATH=$INSTALLROOT   \
+    -DCMAKE_CXX_STANDARD=${CXXSTD}             \
     -DBUILD_SHARED_LIBS=YES                    \
     ${ZLIB_ROOT:+-DZLIB_ROOT:PATH=$ZLIB_ROOT}  \
     -DCMAKE_SKIP_RPATH=YES                     \
-    -DSKIP_INSTALL_FILES=1
-make ${JOBS:+-j $JOBS}
-make install
+    -DSKIP_INSTALL_FILES=1                     \
+    -DCMAKE_INSTALL_LIBDIR=lib
+cmake --build . -- ${JOBS:+-j$JOBS} install
 
 # Modulefile
 MODULEDIR="$INSTALLROOT/etc/modulefiles"
 MODULEFILE="$MODULEDIR/$PKGNAME"
 mkdir -p "$MODULEDIR"
-cat > "$MODULEFILE" <<EoF
-#%Module1.0
-proc ModulesHelp { } {
-  global version
-  puts stderr "ALICE Modulefile for $PKGNAME $PKGVERSION-@@PKGREVISION@$PKGHASH@@"
-}
-set version $PKGVERSION-@@PKGREVISION@$PKGHASH@@
-module-whatis "ALICE Modulefile for $PKGNAME $PKGVERSION-@@PKGREVISION@$PKGHASH@@"
-# Dependencies
-module load BASE/1.0 ${ZLIB_VERSION:+zlib/$ZLIB_VERSION-$ZLIB_REVISION}
-# Our environment
-setenv LIBPNG_ROOT \$::env(BASEDIR)/$PKGNAME/\$version
-prepend-path PATH $::env(LIBPNG_ROOT)/bin
-prepend-path LD_LIBRARY_PATH $::env(LIBPNG_ROOT)/lib
-$([[ ${ARCHITECTURE:0:3} == osx ]] && echo "prepend-path DYLD_LIBRARY_PATH $::env(LIBPNG_ROOT)/lib")
-EoF
+alibuild-generate-module --bin --lib > $MODULEFILE
---
+++ ninja.sh
@@ -0,0 +1,21 @@
+package: ninja
+version: "fortran-%(short_hash)s"
+tag: "v1.11.1.g95dee.kitware.jobserver-1"
+source: https://github.com/Kitware/ninja
+build_requires:
+  - "GCC-Toolchain:(?!osx)"
+  - "CMake"
+  - alibuild-recipe-tools
+prefer_system: .*
+prefer_system_check: |
+  type ninja
+---
+#!/bin/bash
+cmake -Bbuild-cmake "$SOURCEDIR"
+cmake --build build-cmake ${JOBS:+-j$JOBS}
+
+mkdir -p "$INSTALLROOT"/bin
+cp build-cmake/ninja "$INSTALLROOT/bin"
+
+mkdir -p "$INSTALLROOT/etc/modulefiles"
+alibuild-generate-module --bin >"$INSTALLROOT/etc/modulefiles/$PKGNAME"
