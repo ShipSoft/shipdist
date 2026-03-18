d1e55bcc815d956834f6a18fc289295fd05e130b
Oliver Lantwin <oliver.lantwin@cern.ch>
2026-03-18 21:39:24 +0100

    feat: add level-2 individual Python package recipes
    - matplotlib (from arch_x86-64)
    - awkward, uproot (new)

---
+++ python-uproot.sh
@@ -0,0 +1,38 @@
+package: python-uproot
+version: "%(tag_basename)s"
+tag: "v5.7.2"
+source: https://github.com/scikit-hep/uproot5
+requires:
+  - "Python:(slc|ubuntu)"
+  - "Python-system:(?!slc.*|ubuntu)"
+  - python-awkward
+  - python-cramjam
+  - python-fsspec
+  - python-numpy
+  - python-packaging
+  - python-xxhash
+build_requires:
+  - uv
+  - alibuild-recipe-tools
+prefer_system_check: |
+  python3 -c 'import uproot; print(uproot.__version__)' || exit 1
+  SYSTEM_VERSION=$(python3 -c 'import uproot; print(uproot.__version__)')
+  printf '%s\n%s\n' "$PKGVERSION" "$SYSTEM_VERSION" | sort -V -C
+prepend_path:
+  PYTHONPATH: "$PYTHON_UPROOT_ROOT/lib/python/site-packages"
+---
+#!/bin/bash -e
+pyver=$(python3 -c 'import sysconfig; print(sysconfig.get_python_version())')
+TARGET="$INSTALLROOT/lib/python$pyver/site-packages"
+mkdir -p "$TARGET"
+
+uv pip install --no-deps --no-cache-dir --target="$TARGET" --python="$(command -v python3)" "uproot==$PKGVERSION"
+
+ln -snf "python$pyver" "$INSTALLROOT/lib/python"
+
+mkdir -p "$INSTALLROOT/etc/modulefiles"
+alibuild-generate-module > "$INSTALLROOT/etc/modulefiles/$PKGNAME"
+cat >> "$INSTALLROOT/etc/modulefiles/$PKGNAME" <<EOF
+set PKG_ROOT \$::env(BASEDIR)/$PKGNAME/\$version
+prepend-path PYTHONPATH \$PKG_ROOT/lib/python/site-packages
+EOF
