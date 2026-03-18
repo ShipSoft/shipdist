00043dfda8fae65d9c490866b072625b444b9578
Oliver Lantwin <oliver.lantwin@cern.ch>
2026-03-18 21:32:57 +0100

    feat: add level-0 individual Python package recipes
    Add recipes with no Python package dependencies:
    - six, packaging, pyparsing, cycler, fonttools, kiwisolver, pyyaml,
      tabulate (from arch_x86-64)
    - cramjam, xxhash, fsspec (new)

---
+++ python-packaging.sh
@@ -0,0 +1,32 @@
+package: python-packaging
+version: "%(tag_basename)s"
+tag: "26.0"
+source: https://github.com/pypa/packaging
+requires:
+  - "Python:(slc|ubuntu)"
+  - "Python-system:(?!slc.*|ubuntu)"
+build_requires:
+  - uv
+  - alibuild-recipe-tools
+prefer_system_check: |
+  python3 -c 'import packaging; print(packaging.__version__)' || exit 1
+  SYSTEM_VERSION=$(python3 -c 'import packaging; print(packaging.__version__)')
+  printf '%s\n%s\n' "$PKGVERSION" "$SYSTEM_VERSION" | sort -V -C
+prepend_path:
+  PYTHONPATH: "$PYTHON_PACKAGING_ROOT/lib/python/site-packages"
+---
+#!/bin/bash -e
+pyver=$(python3 -c 'import sysconfig; print(sysconfig.get_python_version())')
+TARGET="$INSTALLROOT/lib/python$pyver/site-packages"
+mkdir -p "$TARGET"
+
+uv pip install --no-deps --no-cache-dir --target="$TARGET" --python="$(command -v python3)" "packaging==$PKGVERSION"
+
+ln -snf "python$pyver" "$INSTALLROOT/lib/python"
+
+mkdir -p "$INSTALLROOT/etc/modulefiles"
+alibuild-generate-module > "$INSTALLROOT/etc/modulefiles/$PKGNAME"
+cat >> "$INSTALLROOT/etc/modulefiles/$PKGNAME" <<EOF
+set PKG_ROOT \$::env(BASEDIR)/$PKGNAME/\$version
+prepend-path PYTHONPATH \$PKG_ROOT/lib/python/site-packages
+EOF
---
+++ python-pyyaml.sh
@@ -0,0 +1,32 @@
+package: python-pyyaml
+version: "%(tag_basename)s"
+tag: "6.0.3"
+source: https://github.com/yaml/pyyaml
+requires:
+  - "Python:(slc|ubuntu)"
+  - "Python-system:(?!slc.*|ubuntu)"
+build_requires:
+  - uv
+  - alibuild-recipe-tools
+prefer_system_check: |
+  python3 -c 'import yaml; print(yaml.__version__)' || exit 1
+  SYSTEM_VERSION=$(python3 -c 'import yaml; print(yaml.__version__)')
+  printf '%s\n%s\n' "$PKGVERSION" "$SYSTEM_VERSION" | sort -V -C
+prepend_path:
+  PYTHONPATH: "$PYTHON_PYYAML_ROOT/lib/python/site-packages"
+---
+#!/bin/bash -e
+pyver=$(python3 -c 'import sysconfig; print(sysconfig.get_python_version())')
+TARGET="$INSTALLROOT/lib/python$pyver/site-packages"
+mkdir -p "$TARGET"
+
+uv pip install --no-deps --no-cache-dir --target="$TARGET" --python="$(command -v python3)" "PyYAML==$PKGVERSION"
+
+ln -snf "python$pyver" "$INSTALLROOT/lib/python"
+
+mkdir -p "$INSTALLROOT/etc/modulefiles"
+alibuild-generate-module > "$INSTALLROOT/etc/modulefiles/$PKGNAME"
+cat >> "$INSTALLROOT/etc/modulefiles/$PKGNAME" <<EOF
+set PKG_ROOT \$::env(BASEDIR)/$PKGNAME/\$version
+prepend-path PYTHONPATH \$PKG_ROOT/lib/python/site-packages
+EOF
