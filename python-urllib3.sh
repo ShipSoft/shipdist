5084f18b3c8a8f1f4d94900ae50ab33f626bd741
Oliver Lantwin <oliver.lantwin@cern.ch>
2026-03-20 10:26:36 +0100

    feat: add level-0 dependency recipes for alibuild
    Add python-markupsafe, python-charset-normalizer, python-idna,
    python-urllib3, python-certifi, python-distro.

---
+++ python-urllib3.sh
@@ -0,0 +1,32 @@
+package: python-urllib3
+version: "%(tag_basename)s"
+tag: "2.6.3"
+source: https://github.com/urllib3/urllib3
+requires:
+  - "Python:(slc|ubuntu)"
+  - "Python-system:(?!slc.*|ubuntu)"
+build_requires:
+  - uv
+  - alibuild-recipe-tools
+prefer_system_check: |
+  python3 -c 'import urllib3; print(urllib3.__version__)' || exit 1
+  SYSTEM_VERSION=$(python3 -c 'import urllib3; print(urllib3.__version__)')
+  printf '%s\n%s\n' "$PKGVERSION" "$SYSTEM_VERSION" | sort -V -C
+prepend_path:
+  PYTHONPATH: "$PYTHON_URLLIB3_ROOT/lib/python/site-packages"
+---
+#!/bin/bash -e
+pyver=$(python3 -c 'import sysconfig; print(sysconfig.get_python_version())')
+TARGET="$INSTALLROOT/lib/python$pyver/site-packages"
+mkdir -p "$TARGET"
+
+uv pip install --no-deps --no-cache-dir --target="$TARGET" --python="$(command -v python3)" "urllib3==$PKGVERSION"
+
+ln -snf "python$pyver" "$INSTALLROOT/lib/python"
+
+mkdir -p "$INSTALLROOT/etc/modulefiles"
+alibuild-generate-module > "$INSTALLROOT/etc/modulefiles/$PKGNAME"
+cat >> "$INSTALLROOT/etc/modulefiles/$PKGNAME" <<EOF
+set PKG_ROOT \$::env(BASEDIR)/$PKGNAME/\$version
+prepend-path PYTHONPATH \$PKG_ROOT/lib/python/site-packages
+EOF
