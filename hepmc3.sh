8f13d2279ce9c8cc2aa0b93e681d685d9155a6a2
Oliver Lantwin <oliver.lantwin@cern.ch>
2026-03-22 23:01:17 +0100

    fix: disable HepMC3 Python bindings to avoid installing into system prefix
    The Python bindings try to install into the LCG Python site-packages
    directory which is read-only. Only the C++ libraries are needed.

--- hepmc3.sh
+++ hepmc3.sh
@@ -24,6 +24,7 @@ alibuild-generate-module --bin --lib > "${MODULEFILE}"
 cmake  "$SOURCEDIR"                           \
        -DCMAKE_INSTALL_PREFIX="$INSTALLROOT"  \
        -DCMAKE_INSTALL_LIBDIR=lib           \
+       -DHEPMC3_ENABLE_PYTHON=OFF           \
        -DROOT_DIR="$ROOT_ROOT"
 
 make ${JOBS+-j $JOBS}
