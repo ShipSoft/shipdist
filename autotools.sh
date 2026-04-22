a1dcbca702a8d5d20ad5ff79b509026257b2db76
Oliver Lantwin <oliver.lantwin@cern.ch>
2026-04-22 17:12:30 +0200

    fix(autotools): name missing binary in error
    Interpolate $bin so the error reports which
    specific tool is absent.

--- autotools.sh
+++ autotools.sh
@@ -6,8 +6,8 @@ prefer_system: "(?!slc5|slc6)"
 prefer_system_check: |
   for bin in autoconf m4 automake makeinfo aclocal pkg-config autopoint libtool; do
     if ! command -v "$bin" >/dev/null 2>&1; then
-      cat <<\EOF
-  One or more autotools packages are missing on your system.
+      cat <<EOF
+  $bin is missing on your system.
    * On a RHEL-compatible system you probably need:
      autoconf automake texinfo gettext gettext-devel libtool
    * On an Ubuntu-like system you probably need:
